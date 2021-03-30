27db47886818ca2b6497f72a483dea94c70a4aa5
Oscar Andersson <oscarandersson2@gmail.com>
2021-03-30 11:57:48 +0200

    New migration Fix of problems with migration...

--- Migrations/20210330094657_Init.cs
+++ Migrations/20210330094657_Init.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace Garage_3.Migrations
 {
-    public partial class init : Migration
+    public partial class Init : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
@@ -49,8 +49,7 @@ namespace Garage_3.Migrations
                     Address = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
                     PostNumber = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
                     City = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
-                    Base_Rate = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
-                    Hourly_Rate = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    StayPro = table.Column<bool>(type: "bit", nullable: false),
                     GarageId = table.Column<int>(type: "int", nullable: false)
                 },
                 constraints: table =>
@@ -73,7 +72,8 @@ namespace Garage_3.Migrations
                     IsOccupied = table.Column<bool>(type: "bit", nullable: false),
                     Column_position = table.Column<int>(type: "int", nullable: false),
                     Row_position = table.Column<int>(type: "int", nullable: false),
-                    GarageId = table.Column<int>(type: "int", nullable: false)
+                    GarageId = table.Column<int>(type: "int", nullable: false),
+                    VehicleId = table.Column<int>(type: "int", nullable: true)
                 },
                 constraints: table =>
                 {
@@ -102,7 +102,8 @@ namespace Garage_3.Migrations
                     CheckOutTime = table.Column<DateTime>(type: "datetime2", nullable: false),
                     IsParked = table.Column<bool>(type: "bit", nullable: false),
                     MembershipId = table.Column<int>(type: "int", nullable: false),
-                    VehicleTypeId = table.Column<int>(type: "int", nullable: false)
+                    VehicleTypeId = table.Column<int>(type: "int", nullable: false),
+                    ParkingPlaceId = table.Column<int>(type: "int", nullable: true)
                 },
                 constraints: table =>
                 {
@@ -137,14 +138,12 @@ namespace Garage_3.Migrations
                         name: "FK_ParkingPlaceVehicles_ParkingPlace_ParkingPlaceId",
                         column: x => x.ParkingPlaceId,
                         principalTable: "ParkingPlace",
-                        principalColumn: "ParkingPlaceId",
-                        onDelete: ReferentialAction.NoAction);
+                        principalColumn: "ParkingPlaceId");
                     table.ForeignKey(
                         name: "FK_ParkingPlaceVehicles_Vehicle_VehicleId",
                         column: x => x.VehicleId,
                         principalTable: "Vehicle",
-                        principalColumn: "VehicleId",
-                        onDelete: ReferentialAction.NoAction);
+                        principalColumn: "VehicleId");
                 });
 
             migrationBuilder.InsertData(
@@ -164,51 +163,51 @@ namespace Garage_3.Migrations
 
             migrationBuilder.InsertData(
                 table: "Membership",
-                columns: new[] { "MembershipId", "Address", "Base_Rate", "Birthdate", "City", "FirstName", "GarageId", "Hourly_Rate", "LastName", "Personnummer", "PostNumber", "RegistrationDate" },
+                columns: new[] { "MembershipId", "Address", "Birthdate", "City", "FirstName", "GarageId", "LastName", "Personnummer", "PostNumber", "RegistrationDate", "StayPro" },
                 values: new object[,]
                 {
-                    { 1, "123 Johan St", 1.2m, new DateTime(1991, 3, 29, 15, 37, 26, 832, DateTimeKind.Local).AddTicks(1204), "Stockholm", "Kalle", 1, 2.3m, "Anka", "198706051234", "11111", new DateTime(2021, 3, 29, 15, 37, 26, 830, DateTimeKind.Local).AddTicks(6154) },
-                    { 2, "123 Johan St", 1.2m, new DateTime(1971, 3, 29, 15, 37, 26, 832, DateTimeKind.Local).AddTicks(3113), "Bag End", "Frodo", 1, 2.3m, "Baggins", "198706051234", "22222", new DateTime(2021, 3, 29, 15, 37, 26, 832, DateTimeKind.Local).AddTicks(3104) },
-                    { 3, "123 Johan St", 1.2m, new DateTime(1956, 3, 29, 15, 37, 26, 832, DateTimeKind.Local).AddTicks(3122), "Hobbiton", "Samwise", 1, 2.3m, "Gamgee", "198706051234", "33333", new DateTime(2021, 3, 29, 15, 37, 26, 832, DateTimeKind.Local).AddTicks(3120) },
-                    { 4, "123 Johan St", 1.2m, new DateTime(1951, 3, 29, 15, 37, 26, 832, DateTimeKind.Local).AddTicks(3128), "Stockholm", "Meriadoc", 1, 2.3m, "Brandybuck", "198706051234", "44444", new DateTime(2017, 3, 29, 15, 37, 26, 832, DateTimeKind.Local).AddTicks(3125) }
+                    { 1, "123 Johan St", new DateTime(1991, 3, 30, 11, 46, 56, 706, DateTimeKind.Local).AddTicks(6374), "Stockholm", "Kalle", 1, "Anka", "198706051234", "11111", new DateTime(2021, 3, 30, 11, 46, 56, 702, DateTimeKind.Local).AddTicks(833), false },
+                    { 2, "123 Johan St", new DateTime(1971, 3, 30, 11, 46, 56, 706, DateTimeKind.Local).AddTicks(9283), "Bag End", "Frodo", 1, "Baggins", "198706051234", "22222", new DateTime(2021, 3, 30, 11, 46, 56, 706, DateTimeKind.Local).AddTicks(9253), false },
+                    { 3, "123 Johan St", new DateTime(1956, 3, 30, 11, 46, 56, 706, DateTimeKind.Local).AddTicks(9327), "Hobbiton", "Samwise", 1, "Gamgee", "198706051234", "33333", new DateTime(2021, 3, 30, 11, 46, 56, 706, DateTimeKind.Local).AddTicks(9312), false },
+                    { 4, "123 Johan St", new DateTime(1951, 3, 30, 11, 46, 56, 706, DateTimeKind.Local).AddTicks(9366), "Stockholm", "Meriadoc", 1, "Brandybuck", "198706051234", "44444", new DateTime(2017, 3, 30, 11, 46, 56, 706, DateTimeKind.Local).AddTicks(9351), false }
                 });
 
             migrationBuilder.InsertData(
                 table: "ParkingPlace",
-                columns: new[] { "ParkingPlaceId", "Column_position", "GarageId", "IsOccupied", "Row_position" },
+                columns: new[] { "ParkingPlaceId", "Column_position", "GarageId", "IsOccupied", "Row_position", "VehicleId" },
                 values: new object[,]
                 {
-                    { 18, 3, 1, false, 6 },
-                    { 17, 2, 1, false, 6 },
-                    { 16, 1, 1, false, 6 },
-                    { 15, 5, 1, false, 4 },
-                    { 14, 4, 1, false, 4 },
-                    { 13, 5, 1, false, 3 },
-                    { 12, 4, 1, false, 3 },
-                    { 11, 5, 1, false, 1 },
-                    { 10, 4, 1, false, 1 },
-                    { 8, 2, 1, false, 4 },
-                    { 19, 4, 1, false, 6 },
-                    { 7, 1, 1, false, 4 },
-                    { 6, 3, 1, false, 3 },
-                    { 5, 2, 1, false, 3 },
-                    { 4, 1, 1, false, 3 },
-                    { 3, 3, 1, false, 1 },
-                    { 2, 2, 1, false, 1 },
-                    { 1, 1, 1, false, 1 },
-                    { 9, 3, 1, false, 4 },
-                    { 20, 5, 1, false, 6 }
+                    { 18, 3, 1, false, 6, null },
+                    { 17, 2, 1, false, 6, null },
+                    { 16, 1, 1, false, 6, null },
+                    { 15, 5, 1, false, 4, null },
+                    { 14, 4, 1, false, 4, null },
+                    { 13, 5, 1, false, 3, null },
+                    { 12, 4, 1, false, 3, null },
+                    { 11, 5, 1, false, 1, null },
+                    { 10, 4, 1, false, 1, null },
+                    { 8, 2, 1, false, 4, null },
+                    { 19, 4, 1, false, 6, null },
+                    { 7, 1, 1, false, 4, null },
+                    { 6, 3, 1, false, 3, null },
+                    { 5, 2, 1, false, 3, null },
+                    { 4, 1, 1, false, 3, null },
+                    { 3, 3, 1, false, 1, null },
+                    { 2, 2, 1, false, 1, null },
+                    { 1, 1, 1, false, 1, null },
+                    { 9, 3, 1, false, 4, null },
+                    { 20, 5, 1, false, 6, null }
                 });
 
             migrationBuilder.InsertData(
                 table: "Vehicle",
-                columns: new[] { "VehicleId", "CheckInTime", "CheckOutTime", "Color", "IsParked", "Make", "MembershipId", "Model", "NumberOfWheels", "RegistrationNumber", "VehicleTypeId", "Year" },
+                columns: new[] { "VehicleId", "CheckInTime", "CheckOutTime", "Color", "IsParked", "Make", "MembershipId", "Model", "NumberOfWheels", "ParkingPlaceId", "RegistrationNumber", "VehicleTypeId", "Year" },
                 values: new object[,]
                 {
-                    { 1, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Red", false, "Banana", 1, "Volvo", 4, "asedf", 1, "1993" },
-                    { 2, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Yellow", false, "Banana", 2, "Volvo", 4, "fdsa", 1, "1996" },
-                    { 3, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Blue", false, "F150", 3, "Ford", 4, "zxcve", 2, "2001" },
-                    { 4, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Green", false, "Fancy Car", 4, "Porsche", 4, "qewrty", 1, "2020" }
+                    { 1, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Red", false, "Banana", 1, "Volvo", 4, null, "asedf", 1, "1993" },
+                    { 2, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Yellow", false, "Banana", 2, "Volvo", 4, null, "fdsa", 1, "1996" },
+                    { 3, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Blue", false, "F150", 3, "Ford", 4, null, "zxcve", 2, "2001" },
+                    { 4, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Green", false, "Fancy Car", 4, "Porsche", 4, null, "qewrty", 1, "2020" }
                 });
 
             migrationBuilder.CreateIndex(
